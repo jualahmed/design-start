b20560e8cdd92fa43ed31ca7e2b455534d2d9ae0
jual ahmed <md.jual.ah@gmail.com>
2018-12-30 13:02:41 +0600

    sovel webpack pata issue

---
+++ gulp/tasks/watch.js
@@ -0,0 +1,34 @@
+var gulp = require('gulp'),
+browserSync = require('browser-sync').create();
+
+gulp.task('watch', function() {
+  browserSync.init({
+    notify: false,
+    server: {
+      baseDir: "app"
+    }
+  });
+
+  gulp.watch('./app/index.html', function() {
+    browserSync.reload();
+  });
+
+  gulp.watch('./app/assets/styles/**/*.css', function() {
+    gulp.start('cssInject');
+  });
+
+  gulp.watch('./app/assets/scriptss/**/*.js', function() {
+    gulp.start('scriptsRefresh');
+  })
+
+});
+
+
+gulp.task('cssInject', ['styles'], function() {
+  return gulp.src('./app/temp/styles/styles.css')
+    .pipe(browserSync.stream());
+});
+
+gulp.task('scriptsRefresh', ['scripts'], function() {
+  browserSync.reload();
+});
