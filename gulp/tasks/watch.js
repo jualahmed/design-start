b346864c6c99c4ec4c603006ad01164ae700cd88
techicsdev <ja@sk-associates.org>
2020-02-03 19:07:53 +0600

    make btn

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -2,33 +2,34 @@ var gulp = require('gulp'),
 browserSync = require('browser-sync').create();
 
 gulp.task('watch', function() {
-  browserSync.init({
-    notify: false,
-    server: {
-      baseDir: "app"
-    }
-  });
+	browserSync.init({
+		notify: false,
+		server: {
+			baseDir: "app"
+		},
+		port: 8080
+	});
 
-  gulp.watch('./app/index.html', function() {
-    browserSync.reload();
-  });
+	gulp.watch('./app/index.html', function() {
+		browserSync.reload();
+	});
 
-  gulp.watch('./app/assets/styles/**/*.css', function() {
-    gulp.start('cssInject');
-  });
+	gulp.watch('./app/assets/styles/**/*.css', function() {
+		gulp.start('cssInject');
+	});
 
-  gulp.watch('./app/assets/scriptss/**/*.js', function() {
-    gulp.start('scriptsRefresh');
-  })
+	gulp.watch('./app/assets/scriptss/**/*.js', function() {
+		gulp.start('scriptsRefresh');
+	})
 
 });
 
 
 gulp.task('cssInject', ['styles'], function() {
-  return gulp.src('./app/temp/styles/styles.css')
-    .pipe(browserSync.stream());
+	return gulp.src('./app/temp/styles/styles.css')
+		.pipe(browserSync.stream());
 });
 
 gulp.task('scriptsRefresh', ['scripts'], function() {
-  browserSync.reload();
+	browserSync.reload();
 });
